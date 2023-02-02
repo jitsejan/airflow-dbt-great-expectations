7f5e23bc05f1cd545d32f8155231a2ab9eecc5cf
Jitse-Jan <code@jitsejan.com>
2023-02-02 01:44:15 +0000

    Use refs instead

--- airflow_dbt_great_expectations/models/worlds_model.sql
+++ airflow_dbt_great_expectations/models/worlds_model.sql
@@ -1,17 +1,8 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
 WITH
-    names AS (SELECT * FROM test.world_names),
-    aliases AS (SELECT * FROM test.world_aliases),
+    names AS (SELECT * FROM {{ ref('world_names')}}),
+    aliases AS (SELECT * FROM {{ ref('world_aliases')}}),
 
     joined AS (
         SELECT
